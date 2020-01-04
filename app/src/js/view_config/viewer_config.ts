d3156bedc71c3b00c46cd8c4161cada1fa433176
Gary Chen <gary@datu.ai>
2020-01-03 16:49:13 -0800

    Refactor label3d and tracking structure

--- app/src/js/view_config/viewer_config.ts
+++ app/src/js/view_config/viewer_config.ts
@@ -8,7 +8,7 @@ import {
 
 import Session from '../common/session'
 import * as types from '../common/types'
-import { ImageViewerConfigType, PointCloudViewerConfigType, State, ViewerConfigType } from '../functional/types'
+import { HomographyViewerConfigType, ImageViewerConfigType, PointCloudViewerConfigType, State, ViewerConfigType } from '../functional/types'
 import { Vector3D } from '../math/vector3d'
 import { SCROLL_ZOOM_RATIO } from './image'
 
@@ -303,6 +303,21 @@ export default class ViewerConfigUpdater {
         )
         if (pointCloudZoomAction) {
           Session.dispatch(pointCloudZoomAction)
+        }
+        break
+      case types.ViewerConfigTypeName.HOMOGRAPHY:
+        {
+          const config = this._viewerConfig as HomographyViewerConfigType
+          let zoomRatio = SCROLL_ZOOM_RATIO
+          if (dY < 0) {
+            zoomRatio = 1. / zoomRatio
+          }
+          const newDistance = config.distance * zoomRatio
+          const newConfig = { ...config, distance: newDistance }
+          Session.dispatch(changeViewerConfig(
+            this._viewerId,
+            newConfig
+          ))
         }
         break
     }
